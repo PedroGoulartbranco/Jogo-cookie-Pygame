5ef0e8606513e955b7d624fc274e5ebd18f524cb
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2025-08-27 15:52:15 -0300

    atualização

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ tela= pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 rodando = True
  
+fonte = pygame.font.Font(None, 128)
+
+titulo = 'How Many Cookies ?'
+
+titulo_tranformado = fonte.render(titulo, True, "black")
+
+pote_quase_cheio = pygame.image.load('Biscoitos/bote_quase_cheio.png').convert_alpha()
+
+vidas = 5
+
 while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
 
     tela.fill((222, 184, 135))
+
+    tela.blit(titulo_tranformado, (220, 10))
+
+    tela.blit(pote_quase_cheio, (500, 20))
 
     pygame.display.flip()
 
